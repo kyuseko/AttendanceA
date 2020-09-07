16097e1510f48ea028c89697ffefc8526ef7956c
kyuseko <oyamaa.k-y.n.n.skat.7@docomo.ne.jp>
2020-09-07 16:37:00 +0900

    Finish signup-user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i   # メールアドレスのフォーマットを検証できる
   validates :email, presence: true, length: { maximum: 100 },
-                    format: { with: VALID_EMAIL_REGEX }
-                    uniqueness: true                         # 一意性の検証 他に同じデータがない
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }                
 end
