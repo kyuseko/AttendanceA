17e50fa45e72e547aa0860f3385bc48809682156
kyuseko <oyamaa.k-y.n.n.skat.7@docomo.ne.jp>
2020-09-13 21:52:14 +0900

    Finish edit-attendances

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -7,9 +7,17 @@ class Attendance < ApplicationRecord
 
   # 出勤時間が存在しない場合、退勤時間は無効
   validate :finished_at_is_invalid_without_a_started_at # sがない
+  # 出勤・退勤時間どちらも存在する時、出勤時間より早い退勤時間は無効
+  validate :started_at_than_finished_at_fast_if_invalid
 
   def finished_at_is_invalid_without_a_started_at
     errors.add(:started_at, "が必要です") if started_at.blank? && finished_at.present?
     # どちらも真（true）だった場合エラー 出勤時間が無い、かつ退勤時間が存在する場合
+  end
+
+  def started_at_than_finished_at_fast_if_invalid
+    if started_at.present? && finished_at.present?
+      errors.add(:started_at, "より早い退勤時間は無効です") if started_at > finished_at
+    end
   end
 end
